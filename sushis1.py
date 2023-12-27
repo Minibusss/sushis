6e62c842d76501ac230e103d484aaa51242ffe43
Minibusss <nicolas.cirou@reseau.eseo.fr>
2023-12-27 12:08:15 +0100

    mon premier commit

---
+++ sushis1.py
@@ -0,0 +1,82 @@
+
+intro = """Bonjour,\n
+vous avez une tete a vouloir faire des sushis,\n
+parfait un programme a suhsis commence!!"""
+
+
+cuisson_riz = """
+>>>>>>Pour faire cuire votre riz:\n
+-Rincer le a l'eau froide jusqu'a ce que l'eau de rinçage sois claire.\n
+-Verser le riz et l'eau dans le rice-cooker.\n
+-Quand le riz est cuit, verser le dans un grand récipients.\n
+-Verser le vinaigre a sushis, evanter et melanger sans casser le grain de riz."""
+
+
+
+choix_sushis = """liste des types de maki proposées:
+    \n[1] maki saumon \
+    \n[2] maki crevette \
+    \n[3] maki fromage frais concombre \
+    \n[4] maki ommelette japonaise\
+    \n[5] avocat\
+    \n[6] thon"""
+
+
+
+recette = [ """
+>>>>>>>>Pour confectionner vos makis:
+-coupez les feuilles de nori en deux en les pliants par le milieu\n
+-déroulez la natte de bambou sur votre plan de travail et déposez dessus une demi feuille de nori\n
+-après avoir trempé vos doigts dans le tezu, etalez environ 80 grammes de riz sur la moitié inferieur de la feuille de nori\n
+-deposez une belle lanière de saumon au centre du riz sur la largeur\n
+-roulez la natte en partant du bas et en serrant fermement puis coupez (avec un couteau et du tezu sur la lame) le rouleau obtenue en 6 maki ""","""
+>>>>>>>>Pour confectionner vos makis:
+-coupez les feuilles de nori en deux en les pliants par le milieu\n
+-déroulez la natte de bambou sur votre plan de travail et déposez dessus une demi feuille de nori\n
+-après avoir trempé vos doigts dans le tezu, etalez environ 80 grammes de riz sur la moitié inferieur de la feuille de nori\n
+-deposez une belle lanière de crevette de facon a faire une ligne au centre du riz sur la largeur\n
+-roulez la natte en partant du bas et en serrant fermement puis coupez (avec un couteau et du tezu sur la lame) le rouleau obtenue en 6 maki """, """
+>>>>>>>>Pour confectionner vos makis:
+-coupez les feuilles de nori en deux en les pliants par le milieu\n
+-déroulez la natte de bambou sur votre plan de travail et déposez dessus une demi feuille de nori\n
+-après avoir trempé vos doigts dans le tezu, etalez environ 80 grammes de riz sur la moitié inferieur de la feuille de nori\n
+-étalez du fromage frais sur votre riz
+-deposez une belle lanière de concombre au centre du riz sur la largeur\n
+-roulez la natte en partant du bas et en serrant fermement puis coupez (avec un couteau et du tezu sur la lame) le rouleau obtenue en 6 maki """, """
+>>>>>>>>Pour confectionner vos makis:
+-coupez les feuilles de nori en deux en les pliants par le milieu\n
+-déroulez la natte de bambou sur votre plan de travail et déposez dessus une demi feuille de nori\n
+-après avoir trempé vos doigts dans le tezu, etalez environ 80 grammes de riz sur la moitié inferieur de la feuille de nori\n
+-deposez une belle tranche d'ommelette au centre du riz sur la largeur\n
+-roulez la natte en partant du bas et en serrant fermement puis coupez (avec un couteau et du tezu sur la lame) le rouleau obtenue en 6 maki """, """
+>>>>>>>>Pour confectionner vos makis:
+-coupez les feuilles de nori en deux en les pliants par le milieu\n
+-déroulez la natte de bambou sur votre plan de travail et déposez dessus une demi feuille de nori\n
+-après avoir trempé vos doigts dans le tezu, etalez environ 80 grammes de riz sur la moitié inferieur de la feuille de nori\n
+-deposez une belle lanière d'avocat au entre du riz sur la largeur\n
+-roulez la natte en partant du bas et en serrant fermement puis coupez (avec un couteau et du tezu sur la lame) le rouleau obtenue en 6 maki """, """
+>>>>>>>>Pour confectionner vos makis:
+-coupez les feuilles de nori en deux en les pliants par le milieu\n
+-déroulez la natte de bambou sur votre plan de travail et déposez dessus une demi feuille de nori\n
+-après avoir trempé vos doigts dans le tezu, etalez environ 80 grammes de riz sur la moitié inferieur de la feuille de nori\n
+-deposez une belle lanière de thon au centre du riz sur la largeur\n
+-roulez la natte en partant du bas et en serrant fermement puis coupez (avec un couteau et du tezu sur la lame) le rouleau obtenue en 6 maki """]
+
+
+
+
+
+
+
+
+tips1 = """
+>>>>>>>Pour la suite, veuillez préparer du Tezu.\n
+il s'agit tout simplement d'eau froide et d'un peu de vinaigre a sushis qui serviront a désinfecter le couteau et vos doigts lors de la confection de makis"""
+
+tips2 = """
+>>>>>>>Vous pouvez proposer une sauce soja salée ou sucré pour accompagné vos makis ainsi que du gingembre confis ou du wasabi"""
+
+
+
+
+
